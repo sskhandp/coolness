4f54d391b1dd4f8b4240e3e9188437318164250c
Sundeep Khandpur <sskhandp@gmail.com>
2012-07-11 00:14:17 -0700

    Initial Commit

---
+++ NessChallenge/AppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.m
+//  NessChallenge
+//
+//  Created by Sundeep Khandpur on 7/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+  // Insert code here to initialize your application
+}
+
+@end
---
+++ NessChallenge/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  NessChallenge
+//
+//  Created by Sundeep Khandpur on 7/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+  return NSApplicationMain(argc, (const char **)argv);
+}
